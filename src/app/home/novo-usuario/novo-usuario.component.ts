e315585ba20419197d57591827b249cdc7b38f8b
gabriel <Gabrielvasco906@gmail.com>
2022-07-27 22:27:15 -0300

    validações assincronas

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,8 +1,10 @@
+import { UsuarioExisteService } from './../home-usuario/usuario-existe.service';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
 import { minusculoValidator } from './minusculo.validator';
+
 
 @Component({
   selector: 'app-novo-usuario',
@@ -10,24 +12,26 @@ import { minusculoValidator } from './minusculo.validator';
   styleUrls: ['./novo-usuario.component.css'],
 })
 export class NovoUsuarioComponent implements OnInit {
-  NovoUsuarioForm!: FormGroup;
+  novoUsuarioForm!: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
-    private novoUsuarioService: NovoUsuarioService
+    private novoUsuarioService: NovoUsuarioService,
+    private usuarioExisteService: UsuarioExisteService
   ) {}
 
   ngOnInit(): void {
-    this.NovoUsuarioForm = this.formBuilder.group({
+    this.novoUsuarioForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       fullName: ['', [Validators.required, Validators.minLength(4)]],
-      userName: ['',[minusculoValidator]],
+      userName: ['', [minusculoValidator],[this.usuarioExisteService.usuarioJaExiste()]],
       password: [''],
     });
+
   }
 
   cadastrar() {
-    const novoUsuario = this.NovoUsuarioForm.getRawValue() as NovoUsuario;
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
     console.log(novoUsuario);
   }
 }
